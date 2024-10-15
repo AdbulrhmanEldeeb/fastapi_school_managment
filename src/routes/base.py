c3b2084152678ae0df54716c94f5decec3932e5e
AdbulrhmanEldeeb <abdodebo3@gmail.com>
2024-10-15 18:22:02 +0300

    first deploy commit

--- src/routes/base.py
+++ src/routes/base.py
@@ -3,18 +3,17 @@ from .schemes import Student
 from database import dbmanger
 import os
 
-db_dir_path=r'F:\programing\fast_api\school_system\src\database'
+# db_dir_path=r'F:\programing\fast_api\school_system\src\database'
 
 base_dir = os.path.dirname(__file__)
-relative_db_path = os.path.join(base_dir, '..', 'database', 'studentsdb.db')
-absolute_db_path = os.path.abspath(relative_db_path)
-db_dir_path=r'src\database\studentsdb.db'
+relative_db_path = os.path.join(base_dir, '..', 'database')
+# absolute_db_path = os.path.abspath(relative_db_path)
+db_dir_path=relative_db_path
 base_router=APIRouter() 
 
 @base_router.get('/data',response_model=Student)
 async def get_data(id : int=Query(...,gt=0,description="identifier of student")) -> Student:
     student=dbmanger.db_get_student_data(student_id=id,db_dir_path=db_dir_path)
-    # student=next((s for s in students if s.id==id),None)
     if student is None : 
         raise  HTTPException(status_code=404,detail='this id is not in data')
 
@@ -36,5 +35,5 @@ async def update_student(updated_student_data:Student)-> Student:
 async def delete_student(id : int = Query(...,gt=0,description="identifier of student")):
     dbmanger.db_delete_student(id,db_dir_path=db_dir_path)
     return {
-        f'message':"student with id {id} was deleted successfully"
+        'message':f"student with id {id} was deleted successfully"
         }
